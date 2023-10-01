073cec3a7200ae5edfd6ea6ac2c1740bb41284d5
GastarTestProjects <53343040+GastarTestProjects@users.noreply.github.com>
2023-10-01 14:19:17 +0400

    EnemySpawner Wip. Add game config

--- Assets/_Game/Scripts/Installers/GameConfigInstaller.cs
+++ Assets/_Game/Scripts/Installers/GameConfigInstaller.cs
@@ -1,9 +1,17 @@
-﻿using Zenject;
+﻿using UnityEngine;
+using Zenject;
 
 namespace _Game.Scripts.Installers
 {
+    [CreateAssetMenu(menuName = "Custom/GameConfigInstaller")]
     public class GameConfigInstaller: ScriptableObjectInstaller<GameConfigInstaller>
     {
+        public GameInstaller.Config GameInstaller;
         
+        
+        public override void InstallBindings()
+        {
+            Container.BindInstance(GameInstaller).IfNotBound();
+        }
     }
 }
